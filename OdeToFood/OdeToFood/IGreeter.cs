2b2356b34074a977d29095dd1c3c0cfafca8137b
Francine Taylor <ftaylor1960@yahoo.com>
2019-03-06 23:56:20 -0800

    My first check-in

---
+++ OdeToFood/OdeToFood/IGreeter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood
+{
+    public interface IGreeter
+    {
+        string GetMessageOfTheDay();
+    }
+    public class Greeter : IGreeter
+    {
+        string _configMessage = "Greetings";
+        public Greeter(IConfiguration configuration)
+        {
+            string greeting = configuration["Greeting"];
+            if (!string.IsNullOrWhiteSpace(greeting))
+            {
+                _configMessage = greeting;
+            }
+        }
+        public string GetMessageOfTheDay()
+        {
+            return _configMessage;
+        }
+    }
+}
